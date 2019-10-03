2449c479d0d259a93f191780a578a1b2d7a462e9
jhkim8268@gmail.com <jhkim8268@gmail.com>
2019-10-03 16:24:48 -0700

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// write your code here
+const http = require('http');
+const port = 8080;
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, 'Ok', {
+    'Content-Type': 'text/plain'
+  });
+  res.write('Hello World', 'utf8');
+  res.end();
+})
+
+app.listen(port);
